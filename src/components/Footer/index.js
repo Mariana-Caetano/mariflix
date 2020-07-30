94583157eacc1585241be6e20365ac1492d8ec2d
Mariana <nanah.caetano@gmail.com>
2020-07-29 23:37:07 -0300

    Create index.js

---
+++ src/components/Footer/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { FooterBase } from './styles';
+import Logo from '../../assets/img/Logo.png'
+
+function Footer() {
+  return (
+    <FooterBase>
+      <a href="https://www.linkedin.com/in/mariana-caetano1/">
+      <img className="Logo" src={Logo} alt="MariFlix Logo" />
+      </a>
+      <p>
+        Orgulhosamente criado durante a
+        {' '}
+        <a href="https://www.alura.com.br/">
+          Imersão React da Alura
+        </a>
+      </p>
+    </FooterBase>
+  );
+}
+
+export default Footer;
